39a9fa3a15fd519ffffe48de132f77deb34e45d7
bleaz01 <ridjea@gmail.com>
2020-01-08 19:27:46 +0100

    add appbar + body de la homepage

--- agences_voyage/lib/main.dart
+++ agences_voyage/lib/main.dart
@@ -7,9 +7,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner:
+          false, // debugShowCheckedModeBanner: false sert a retirer le banner de debug
       title: 'voyages',
       theme: ThemeData(primarySwatch: Colors.blue),
-      
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.home),
+          title: Text('Travel & Dream'),
+          actions: <Widget>[Icon(Icons.more_horiz)],
+        ),
+        body: Container(
+          child: Text('Take your travel'),
+        ),
+      ),
     );
   }
 }
